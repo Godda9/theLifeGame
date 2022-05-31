8a70e09c944842a277365dc40719f4f64dae8327
Гребенец Саша <grebinec.oleksandr@gmail.com>
2022-05-31 14:50:26 +0300

    finished

--- main.py
+++ main.py
@@ -40,15 +40,105 @@ def matrix_input(w: int, h: int):
                 else:
                     arr1[inp[0]][inp[1]] = 1
                     break
+
+            # print(*arr1, sep='\n')
+
+
+def get_sosedi(array: list, i: int, j: int, width: int, height: int):
+    count = 0
+    if j + 1 <= width - 1:
+        if array[i][j + 1] == 1:
+            count += 1
+            # print(f"IF 1 right ")   # +++++++
+
+    if j - 1 >= 0:
+        if array[i][j - 1] == 1:
+            count += 1
+            # print(f"IF 2 left ")   # +++++++
+
+    if i + 1 <= height - 1:
+        if array[i + 1][j] == 1:
+            count += 1
+            # print(f"IF 3 down:")   # +++++++
+
+    if i - 1 >= 0:
+        if array[i - 1][j] == 1:
+            count += 1
+            # print(f"IF 4 up")   # +++++++
+
+    # main diag
+    if (j + 1 <= width - 1) and (i - 1 >= 0):
+        if array[i - 1][j + 1] == 1:
+            count += 1
+            # print(f"IF 5 right+up")  # +++++++
+
+    if j - 1 >= 0 and i + 1 <= height - 1:
+        if array[i + 1][j - 1] == 1:
+            count += 1
+            # print(f"IF 6 left+down")   # +++++++
+
+    # pob diag
+    if (j - 1 >= 0) and (i - 1 >= 0):
+        if array[i - 1][j - 1] == 1:
+            count += 1
+            # print(f"IF 7 left+up")  # +++++++
+
+    if j + 1 <= width - 1 and i + 1 <= height - 1:
+        if array[i + 1][j + 1] == 1:
+            count += 1
+            # print(f"IF 8 right+down")   # +++++++
+    return count
+
+
 def main():
     os.system('cls')
     print("[!] Enter matrix width: ", end='')
     width = int(input())
+    if width <= 0:
+        if width == 0:
+            width += 5
+        else:
+            width = abs(width)
+
     print("[!] Enter matrix height: ", end='')
     height = int(input())
-     # matrix input
+    if height <= 0:
+        if height == 0:
+            height += 5
+        else:
+            height = abs(height)
+    print()
+
+    # matrix input
     arr = matrix_input(w=width, h=height)      # arr = list returned from matrix_input
-    
+
+    while True:
+        alive = 0
+        for i in range(height):
+            for j in range(width):
+                temp = get_sosedi(arr, i, j, width, height)      # count all nearest cells
+
+                if (temp < 2 or temp > 3) and arr[i][j] == 1:    # rule 1
+                    arr[i][j] = 0
+
+                if arr[i][j] == 1 and (temp == 2 or temp == 3):  # rule 2
+                    continue
+
+                if temp == 3 and arr[i][j] == 0:                 # rule 3
+                    arr[i][j] = 1
+
+            asyncio.run(asyncio.sleep(0.3))  # main thread sleep for 0.3 sec
+            os.system('cls')   # clear console
+            print(*arr, sep='\n')
+
+        for i in range(0, height):       #
+            for j in range(0, width):    # just to calculate count (alive)
+                if arr[i][j] == 1:       #
+                    alive += 1           #
+
+        if alive == 0:
+            break
+
 
 if __name__ == '__main__':
     main()
